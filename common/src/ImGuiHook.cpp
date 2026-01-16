d819f8c2eafec990ed0263503554acf9dcce8abb
Alexander Akhundzhanov <kerrytazi@mail.ru>
2026-01-16 04:41:11 +0300

    Use docked ImGui

--- common/src/ImGuiHook.cpp
+++ common/src/ImGuiHook.cpp
@@ -54,6 +54,7 @@ static void ImGuiInitBasic()
 	ImGuiIO& io = ImGui::GetIO(); (void)io;
 	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
 	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
 
 	io.IniFilename = nullptr;
 	io.LogFilename = nullptr;
